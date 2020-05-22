476f87e5079152268912e8f6d3fa61cc21860163
inditechie <65787393+inditechie@users.noreply.github.com>
2020-05-23 01:30:15 +0530

    T-SQL Script

---
+++ DBMirroringLocalServiceAccount.sql
@@ -0,0 +1,76 @@
+/*
+Note: 
+	Port for Principal & Mirror: "5022"
+	
+By:
+	Ghufran Khan
+	FB: https://www.facebook.com/GhufranKhan89
+	Youtube: https://youtu.be/O2RaZpSRC2U
+	GitHub: https://github.com/inditechie?tab=repositories
+*/
+ 
+-- First Step
+-- Run on Primary/Principal node :
+ 
+CREATE ENDPOINT [Mirroring]
+    AS TCP (LISTENER_PORT = 5022)
+    FOR DATA_MIRRORING (
+	    ROLE = ALL,
+	    AUTHENTICATION = WINDOWS NEGOTIATE,
+		ENCRYPTION = REQUIRED ALGORITHM AES
+		);
+ 
+ALTER ENDPOINT [Mirroring] STATE = STARTED;
+GO
+ 
+CREATE LOGIN [<Domain\SecondaryNode>$] FROM WINDOWS -- Example: [CONTOSO\N2$]
+GO
+ 
+GRANT CONNECT ON ENDPOINT::[Mirroring] TO [<Domain\SecondaryNode>$]
+GO
+ 
+-- Second Step
+-- Run on Secondary/Mirror node :
+ 
+ CREATE ENDPOINT [Mirroring]
+    AS TCP (LISTENER_PORT = 5022)
+    FOR DATA_MIRRORING (
+	    ROLE = ALL,
+	    AUTHENTICATION = WINDOWS NEGOTIATE,
+		ENCRYPTION = REQUIRED ALGORITHM AES
+		);
+ 
+ALTER ENDPOINT [Mirroring] STATE = STARTED;
+GO
+ 
+CREATE LOGIN [<Domain\PrimaryNode>$] FROM WINDOWS -- Example: [CONTOSO\N1$]
+GO
+ 
+GRANT CONNECT ON ENDPOINT::[Mirroring] TO [<Domain\PrimaryNode>$]
+GO
+ 
+-- Third Step :
+/*
+Take Full & Log backup of the Mirroring database from Primary/Principal node
+ 
+Restore the Full & Log backup with "NORECOVERY" option on the Secondary/Mirror node
+*/
+ 
+-- Fourth Step:
+-- Run on Secondary/Mirror node :
+USE [master]
+GO
+ 
+ALTER DATABASE [<Database Name>] SET PARTNER = 'TCP://<IP of Primary/Principal node>:5022'
+GO
+ 
+-- Fifth Step
+-- Run on Primary/Principal node :
+USE [master]
+GO
+ 
+ALTER DATABASE [<Database Name>] SET PARTNER = 'TCP://<IP of Secondary/Mirror node>:5022'
+GO
+ 
+ALTER DATABASE [<Database Name>] SET SAFETY FULL -- Synchronous Mode
+GO
